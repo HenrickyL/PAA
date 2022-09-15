ee17d88b78468202e25dfd8c9d480ea82d739beb
HenrickyL <Henrickyl1@gmail.com>
2022-09-14 22:42:50 -0300

    teste 1 trabalho

---
+++ Trabalho/1.Proposta/test.ts
@@ -0,0 +1,11 @@
+function longestPalindrome(s: String){
+
+}
+
+function _longestPalindrome(s: String){
+
+}
+
+function _isPalindrome(s:String, memo?: boolean[][]){
+
+}
---
+++ Trabalho/1.Proposta/teste.py
@@ -0,0 +1,68 @@
+from itertools import count
+from typing import List
+class Solution:
+    countPalind = 0
+    count = 0
+
+    def longestPalindrome(self,s: str):
+        memo = {}
+        self.countPalind = 0
+        self.count = 0
+        if(self._isPalindrom(s,memo)):
+            return s
+        return self._longestPalindrome(s,memo)
+
+
+    def _isPalindrom(self, s:str, memo: dict):
+        self.countPalind += 1
+        if(memo.get(s)!=None):
+            return True
+        else:
+            if(len(s)==1 or (len(s)==2 and s[0]==s[-1])): 
+                return True
+            elif(s[0]==s[-1]):
+                isPalind = self._isPalindrom(s[1:-1],memo)
+                if(isPalind):
+                    memo[s[1:-1]] = len(s[1:-1])
+                    memo[s] = len(s)
+                    print(memo)
+                    return True
+            return False
+            
+
+    def _longestPalindrome(self, s: str, memo:dict):
+        self.count+=1
+        if(memo.get(s) != None):
+            return s
+        if(len(s) > 2):
+            if(s[0] == s[-1]):
+                if(memo.get(s[1:-1]) != None or self._isPalindrom(s[1:-1],memo)):
+                    return s                    
+            x = self._longestPalindrome(s[1:],memo) #bcd
+            y = self._longestPalindrome(s[:-1],memo)#abc
+            return  x if len(x)> len(y) else y 
+        return s if s[0]==s[-1] else s[0]
+
+S = Solution()
+# print(S.longestPalindrome("xabba"))
+# print(S.countPalind, S.count)
+# print(S.count)
+# print(S.longestPalindrome("xabba"))
+# print(S.count)
+print(S.longestPalindrome("zABBAzzABAtu_ABcBA_"))
+print(S.countPalind, S.count)
+
+# print(S.longestPalindrome("cbbd"))
+# print(S.countPalind, S.count)
+
+
+
+
+
+
+
+
+
+
+
+
