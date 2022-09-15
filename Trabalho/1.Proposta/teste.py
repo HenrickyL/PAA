9a10c96cde49f2fb5cac5a9ba81e69184b942b02
HenrickyL <Henrickyl1@gmail.com>
2022-09-14 23:18:24 -0300

    version 1

--- Trabalho/1.Proposta/teste.py
+++ Trabalho/1.Proposta/teste.py
@@ -15,17 +15,20 @@ class Solution:
 
     def _isPalindrom(self, s:str, memo: dict):
         self.countPalind += 1
+        if(len(s)==0): return False
         if(memo.get(s)!=None):
             return True
         else:
             if(len(s)==1 or (len(s)==2 and s[0]==s[-1])): 
                 return True
             elif(s[0]==s[-1]):
+                if(memo.get(s[1:-1])!= None):
+                    memo[s] = len(s)
+                    return True
                 isPalind = self._isPalindrom(s[1:-1],memo)
                 if(isPalind):
                     memo[s[1:-1]] = len(s[1:-1])
                     memo[s] = len(s)
-                    print(memo)
                     return True
             return False
             
@@ -37,7 +40,13 @@ class Solution:
         if(len(s) > 2):
             if(s[0] == s[-1]):
                 if(memo.get(s[1:-1]) != None or self._isPalindrom(s[1:-1],memo)):
-                    return s                    
+                    return s  
+            # if(self._isPalindrom(s[1:],memo)):
+            #     return s[1:]                  
+            # elif(self._isPalindrom(s[:-1],memo)) :
+            #     return s[:-1]                  
+            # elif(self._isPalindrom(s[1:-1],memo)):
+            #     return s[1:-1]                  
             x = self._longestPalindrome(s[1:],memo) #bcd
             y = self._longestPalindrome(s[:-1],memo)#abc
             return  x if len(x)> len(y) else y 
@@ -49,12 +58,22 @@ S = Solution()
 # print(S.count)
 # print(S.longestPalindrome("xabba"))
 # print(S.count)
-print(S.longestPalindrome("zABBAzzABAtu_ABcBA_"))
-print(S.countPalind, S.count)
+# print(S.longestPalindrome("zABBAzzABAtu_ABcBA_"))
+# print(S.countPalind, S.count)
 
 # print(S.longestPalindrome("cbbd"))
 # print(S.countPalind, S.count)
+memo = {}
+print(S._isPalindrom("abcdedcba",memo))
+print(S._isPalindrom("bcdedcb",memo))
+print(S._isPalindrom("cdedc",memo))
+print(S.countPalind, S.count)
 
+
+print(S.longestPalindrome("abb"))
+print(S.countPalind,'\t\t', S.count)
+print(S.longestPalindrome("zABBAzzABAtu_ABcBA_"))
+print(S.countPalind,'\t\t', S.count)
 
 
 
